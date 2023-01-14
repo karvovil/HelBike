1986869099c0f629d85a07405a214067e21eadae
Ville Karvonen <villekarvonen1@gmail.com>
2023-01-14 16:53:30 +0200

    added route for fetching a station

--- server/routes/stations.ts
+++ server/routes/stations.ts
@@ -13,4 +13,10 @@ router.get('/', (_req, res) => {
   res.send(stations);
 });
 
+router.get('/:id', (req, res) => {
+  const name = req.params.id;
+  const station = stations.find(s => s.name === name);
+  res.send(station);
+});
+
 export default router;

2411d21e74a5a545bea84fe71f1dd1b07f9df192
Ville Karvonen <villekarvonen1@gmail.com>
2023-07-07 11:05:18 +0300

    pure part of JourneyList moved to new component

---
+++ client/src/components/JourneyList/JourneyListContainer.tsx
@@ -0,0 +1,63 @@
+import Journey from "./Journey"
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import Paper from '@mui/material/Paper';
+import JourneyHeaders from "./JourneyListHeaders";
+import { TableFooter, TableRow, TablePagination } from "@mui/material";
+import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
+import { BaseJourney, Order } from "../../types";
+
+interface JourneyListContainerProps {
+	journeys: BaseJourney[],
+  currentPage: number,
+  orderBy: string,
+  orderDirection: Order
+  rowCount: number,
+	rowsPerPage: number,
+  handlePageChange:(event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => void,
+  handleSortClick: (orderAttribute: string) => void,
+  handleChangeRowsPerPage: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+}
+const JourneyListContainer = ({
+  journeys, currentPage, orderBy, orderDirection, rowCount,
+  rowsPerPage, handlePageChange, handleSortClick, handleChangeRowsPerPage, 
+}:JourneyListContainerProps) => {
+
+  return(
+    <Paper sx={{ width: '100%', mb: 2 }}>
+
+      <TableContainer >
+        <Table 
+          sx={{ minWidth: 500 }}
+          size="small"
+        >
+          <JourneyHeaders
+            orderBy={orderBy}
+            orderDirection={orderDirection}
+            onHandleSortClick={handleSortClick}
+          />
+          <TableBody>
+            {journeys.map(j => <Journey
+              key={j.id}
+              journey={j} />)}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                count={rowCount}
+                page={currentPage}
+                rowsPerPage={rowsPerPage}
+                onPageChange={handlePageChange}
+                ActionsComponent={TablePaginationActions}
+                rowsPerPageOptions={[50, 100, 200]}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
+}
+export default JourneyListContainer
--- client/src/components/JourneyList/index.tsx
+++ client/src/components/JourneyList/index.tsx
@@ -1,15 +1,9 @@
 import { BaseJourney, Order } from "../../types"
-import Journey from "./Journey"
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableContainer from '@mui/material/TableContainer';
-import Paper from '@mui/material/Paper';
-import JourneyHeaders from "./JourneyListHeaders";
-import { TableFooter, TableRow, TablePagination } from "@mui/material";
-import TablePaginationActions from "@mui/material/TablePagination/TablePaginationActions";
+
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import JourneyListContainer from "./JourneyListContainer";
 
 const JourneyList = () => {
   const [journeys, setJourneys] = useState<BaseJourney[]>([])
@@ -36,7 +30,7 @@ const JourneyList = () => {
     event: React.MouseEvent<HTMLButtonElement> | null,
     newPage: number,
   ) => setCurrentPage(newPage)
-
+        
   const handleSortClick = (orderAttribute: string) =>  {
     if (orderBy === orderAttribute){
       orderDirection === 'asc' ? setOrder('desc') : setOrder('asc')
@@ -52,40 +46,18 @@ const JourneyList = () => {
     setCurrentPage(0);
   };
 
-  return(
-    <Paper sx={{ width: '100%', mb: 2 }}>
-
-      <TableContainer >
-        <Table 
-          sx={{ minWidth: 500 }}
-          size="small"
-        >
-          <JourneyHeaders
-            orderBy={orderBy}
-            orderDirection={orderDirection}
-            onHandleSortClick={handleSortClick}
-          />
-          <TableBody>
-            {journeys.map(j => <Journey
-              key={j.id}
-              journey={j} />)}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                count={rowCount}
-                page={currentPage}
-                rowsPerPage={rowsPerPage}
-                onPageChange={handlePageChange}
-                ActionsComponent={TablePaginationActions}
-                rowsPerPageOptions={[50, 100, 200]}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </TableContainer>
-    </Paper>
-  );
+  return (
+    <JourneyListContainer
+      journeys={journeys}
+      currentPage={currentPage}
+      orderBy={orderBy}
+      orderDirection={orderDirection}
+      rowCount={rowCount}
+      rowsPerPage={rowsPerPage}
+      handlePageChange={handlePageChange}
+      handleSortClick={handleSortClick}
+      handleChangeRowsPerPage={handleChangeRowsPerPage}
+    />
+  )
 }
 export default JourneyList

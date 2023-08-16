96c19ec75037e377346f7fc7c31dee34b148f70b
Ville Karvonen <villekarvonen1@gmail.com>
2023-08-16 11:08:40 +0300

    dev mode is able to create db from test data if there is no existing db and datafiles are missing

--- util/db.ts
+++ util/db.ts
@@ -4,6 +4,7 @@ import { Journey, Station } from '../models';
 import * as fs from "fs";
 import { DBPATH } from './config';
 import {testStations, testJourneys} from '../testData';
+import * as path from "path";
 
 export const sequelize = new Sequelize(
   'bikeJourneyDB',
@@ -17,8 +18,10 @@ export const sequelize = new Sequelize(
 
 export const connectToDB = async () => {
   const dbExists = fs.existsSync(DBPATH);
-  console.log("dbExists: " + dbExists);
-  
+  if(dbExists){
+    console.log('using existing db');
+  }
+
   try {
     await sequelize.authenticate();
     console.log('Connected to DB');
@@ -34,34 +37,36 @@ export const connectToDB = async () => {
 };
 
 export const populateDatabase = async () => {
-  
-  if (process.env.NODE_ENV === 'test'){
+  const dataFiles = [
+    'Helsingin_ja_Espoon_kaupunkipy%C3%B6r%C3%A4asemat_avoin.csv',
+    '2021-05.csv','2021-06.csv','2021-07.csv']
+    .map(f => path.resolve('files', f));
+    
+  const dataFilesExist = dataFiles.reduce((a,b) => a && fs.existsSync(b), true); 
+    
+  if (process.env.NODE_ENV === 'test' || !dataFilesExist){
     await Station.bulkCreate(testStations);
     await Journey.bulkCreate(testJourneys);
-    console.log('populating testDB');    
+    console.log('using test data');    
   }else{
-    
     try {
-      const stations = await parseStations(
-        '../files/Helsingin_ja_Espoon_kaupunkipy%C3%B6r%C3%A4asemat_avoin.csv'
-      );
+      const stations = await parseStations(dataFiles[0]);
       await Station.bulkCreate(stations);
       const stationIds = stations.map(s => s.id);
 
       console.log('parsing journeys 1/3');
       await Journey.bulkCreate(
-        await parseJourneys('../files/2021-05.csv', stationIds)
+        await parseJourneys(dataFiles[1], stationIds)
       );  
-
       console.log('parsing journeys 2/3');
       await Journey.bulkCreate(
-        await parseJourneys('../files/2021-06.csv', stationIds)
+        await parseJourneys(dataFiles[2], stationIds)
       );  
-
       console.log('parsing journeys 3/3');
       await Journey.bulkCreate(
-        await parseJourneys('../files/2021-07.csv', stationIds)
+        await parseJourneys(dataFiles[3], stationIds)
       );  
+      console.log('using data parsed from .csv files');
       
     } catch (err) {
       console.log(err);

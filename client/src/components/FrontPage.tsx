131a5699ee2027cae0964d260f66862ba27eeafb
Ville Karvonen <villekarvonen1@gmail.com>
2023-09-28 10:45:33 +0300

    frontpage has short introduction text

--- client/src/components/FrontPage.tsx
+++ client/src/components/FrontPage.tsx
@@ -3,15 +3,19 @@ import { Box, Typography } from '@mui/material';
 const FrontPage = () => {
   return (
     <Box sx={{ width: '45%', margin: 'auto', align: 'center' }}>
+      <Typography variant='h4' align='center' fontWeight={600}>
+        HSL city bike stations and journeys
+      </Typography>
       <Box
         component='img'
         src={process.env.PUBLIC_URL + '/favicon.ico'}
         title='logo'
-        sx={{ width: '100%', margin: 'auto' }}
-      />
-      <Typography variant='h4' align='center' fontWeight={600}>
-        HSL city bike stations and journeys
+        sx={{ width: '100%', margin: 'auto' }} />
+      <Typography align='center' fontStyle='oblique' fontSize={12}>
+        Service for displaying data from journeys made with city bikes in the Helsinki Capital area.
+        Data includes journeys made in 2021 and is owned by <a href="https://www.citybikefinland.fi/">City Bike Finland</a>
       </Typography>
     </Box>
-  ) }
+  );
+}
 export default FrontPage
